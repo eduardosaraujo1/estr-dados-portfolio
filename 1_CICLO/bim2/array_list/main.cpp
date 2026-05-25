38afc17698961175132b6c99b70d64b7efbfa5a7
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-05-25 17:58:57 -0300

    feat: ordered list

---
+++ 1_CICLO/bim2/array_list/main.cpp
@@ -0,0 +1,77 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+class ListaDeArray {
+private:
+  int *VET;
+  int ProximaPosicaoLivre;
+
+public:
+  ListaDeArray(int qtde) {
+    VET = new int[qtde];
+    ProximaPosicaoLivre = 0;
+  }
+  void Insere(int n) {
+    int pos = ProximaPosicaoLivre - 1;
+    while ((n < VET[pos]) && (pos > -1)) {
+      VET[pos + 1] = VET[pos];
+      pos--;
+    }
+    VET[pos + 1] = n;
+    ProximaPosicaoLivre++;
+  }
+  void Mostra() {
+    for (int i = 0; i < ProximaPosicaoLivre; i++) {
+      cout << VET[i] << "\n";
+    }
+  }
+  int Busca(int n) {
+    cout << "Iniciando busca" << '\n';
+    int i = 0;
+    int j = ProximaPosicaoLivre - 1;
+    while (i < j) {
+      int meio = (i + j + 1) / 2;
+      cout << "i=" << i << ";j=" << j << ";meio=" << meio << endl;
+      if (VET[meio] == n) {
+        return n;
+      }
+
+      if (VET[meio] > n) {
+        j = meio;
+      } else {
+        i = meio;
+      }
+    }
+    // for (int i = 0; i < ProximaPosicaoLivre; i++) {
+    //   if (n == VET[i])
+    //     return i;
+    // }
+    return -1;
+  }
+  void Remove(int n) {
+    int pos = Busca(n);
+    if (pos > -1) {
+      for (int i = pos + 1; i < ProximaPosicaoLivre; i++) {
+        VET[i - 1] = VET[i];
+      }
+      ProximaPosicaoLivre--;
+    }
+  }
+};
+
+int main(int argc, char *argv[]) {
+  ListaDeArray lista(50);
+  lista.Insere(1);
+  lista.Insere(12);
+  lista.Insere(3);
+  lista.Insere(7);
+  lista.Remove(200);
+  lista.Remove(3);
+  lista.Mostra();
+  cout << "\n\n\n";
+  cout << lista.Busca(5) << "\n\n\n";
+  // system("PAUSE");
+  return EXIT_SUCCESS;
+}
